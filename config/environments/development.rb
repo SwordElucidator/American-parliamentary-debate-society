fb41319602e9a642382a83de4731696898f7d64b
Sword Elucidator <nagisa940216@gmail.com>
2016-03-11 19:01:17 +0000

    post by user

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,5 +40,5 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   
   #needed for devise   In production, :host should be set to the actual host of your application.
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://cs169-swordelucidator.c9users.io' }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160311153507) do
+ActiveRecord::Schema.define(version: 20160311154322) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
