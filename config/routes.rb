5d978d5681d33b7b5d703f4fccbafabfd45f45cf
Sword Elucidator <nagisa940216@gmail.com>
2016-03-11 20:49:13 +0000

    add comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   
   root 'posts#index'
   # The priority is based upon order of creation: first created -> highest priority.
