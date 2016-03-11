7072800d11832111d0d27e63e5d70bdd673c8b08
Zhongliu <leezhongliu@gmail.com>
2016-03-11 05:20:08 +0000

    add login.js and login.css

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ActiveRecord::Base
        elsif username.length >= 10
            errors.add(:username, 'should not be longer than 10 characters')
            puts "exceed maximum length"
+       elsif User.find_by_username(username)
+           errors.add(:username, 'has already been registered')
        else
            puts "username correct"
        end
