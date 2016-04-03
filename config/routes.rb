bfaf29409ac1a253a068e7bea86f852a641664f6
Zhongliu <leezhongliu@gmail.com>
2016-04-03 01:10:15 +0000

    add a simple UI of invitation code part

--- config/routes.rb
+++ config/routes.rb
@@ -20,11 +20,8 @@ Rails.application.routes.draw do
   #post '/login', to: 'users#login'
   get '/forum', to: "posts#index"
   post '/forum', to: "posts#index"
-<<<<<<< HEAD
-  get '/invitation', to: "invitation#index"
-  post '/invitation', to: "invitation#index"
-=======
   get '/profile', to: "profile#profile"
   post '/profile', to: "profile#profile", as: 'update_profile'
->>>>>>> fd924f5fa18dd589f176c22d82b9457392305b99
+  get '/invitation', to: "invitation#index"
+  post '/invitation', to: "invitation#index"
 end
