363367d22334b34ae585fd21aa4f2ea36c9a9d62
Sword Elucidator <nagisa940216@gmail.com>
2016-04-18 21:58:15 +0000

    add profile features

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -3,12 +3,14 @@ class ProfileController < ApplicationController
     
     def profile
         data = params[:user]
+        @user_to_see = current_user
         if not data
             render 'profile'
         else
             firstname = data[:firstname]
             lastname = data[:lastname]
             major = data[:major]
+            image_url = data[:image_url]
             if firstname and firstname != ""
                 current_user.firstname = firstname
             end
@@ -18,9 +20,18 @@ class ProfileController < ApplicationController
             if major and major != ""
                 current_user.major = major
             end
+            if image_url and image_url != ""
+                current_user.image_url = image_url
+            end
             current_user.save
             render 'profile'
         end
+    end
+    
+    def user
+        id = params[:id]
+        @user_to_see = User.find_by_id(id)
+        render 'profile'
     end
 
 end
