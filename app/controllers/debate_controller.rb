fd90f1d4a55d4e95931687b570f3b6f921a4eb89
Zhongliu <leezhongliu@gmail.com>
2016-04-26 23:58:45 +0000

    user cannot register the debate happened at the same time right now

--- app/controllers/debate_controller.rb
+++ app/controllers/debate_controller.rb
@@ -22,8 +22,12 @@ class DebateController < ApplicationController
             registeredslot = Slot.find_by_id(slotid)
             if checkprofile
                 if checkregister(@debateid)
-                   register(registeredslot)
-                   @error = "nonerror"
+                    if checktime(@debateid)
+                       register(registeredslot)
+                       @error = "nonerror"
+                    else
+                       @error = "timeConflictError"
+                    end
                 else
                    @error = "registerConflictError"
                 end
@@ -102,6 +106,17 @@ class DebateController < ApplicationController
     
     private
     
+    def checktime(debateid)
+        debate = Debate.find_by_id(debateid)
+        if current_user.debates != nil and current_user.debates.find_by_time(debate.time)
+            puts "there is a time conflict between these two debates"
+            flash.delete :success if flash[:success]
+            flash.now[:error] = "You cannot register this debate since it has a time conflict with your registered debate"
+            return false
+        else
+            return true
+        end
+    end
     
     def checkregister(debateid)
         debateslots = Debate.find_by_id(debateid).slots
@@ -121,7 +136,7 @@ class DebateController < ApplicationController
         end
         flash.delete :success if flash[:success]
         flash.now[:error] = "You need to edit your profile first to sign up for a debate."
-        return true
+        return false
     end
     
     def register(registeredslot)
