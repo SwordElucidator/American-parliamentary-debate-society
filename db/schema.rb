c605a50a5a1d19bfa1a9bb27534c3228082d2f1a
Sword Elucidator <nagisa940216@gmail.com>
2016-04-29 09:46:41 +0000

    solve forum issue

--- db/schema.rb
+++ db/schema.rb
@@ -90,27 +90,27 @@ ActiveRecord::Schema.define(version: 20160424161330) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",                                    null: false
-    t.string   "encrypted_password",     default: "",                                    null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string   "username"
     t.string   "password"
-    t.integer  "sign_in_count",          default: 0,                                     null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                                             null: false
-    t.datetime "updated_at",                                                             null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "firstname"
     t.string   "lastname"
     t.string   "major"
     t.datetime "time"
     t.boolean  "is_admin"
     t.string   "code"
-    t.string   "image_url",              default: "http://www.aaea.com.ar/img/user.png"
+    t.string   "image_url"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
